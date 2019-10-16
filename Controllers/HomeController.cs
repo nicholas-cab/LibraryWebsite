dd825adfe51e6ec453e6babd6b23edbae7b16542
nicholas-cab <nicholas121919@gmail.com>
2019-10-15 21:35:31 -0500

    Update HomeController.cs
    added space

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace LibraryWebsite.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
+            return View();    
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
